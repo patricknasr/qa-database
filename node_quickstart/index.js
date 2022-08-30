3bef595e205aac3ed007b038d4f5618c1e51e1da
Patrick Nasr <nasr7601@hotmail.com>
2022-08-30 16:11:05 +1000

    tue 27

--- node_quickstart/index.js
+++ node_quickstart/index.js
@@ -22,30 +22,3 @@ Lender.find()
 
 
 
-
-    
-
-/*const { MongoClient } = require("mongodb");
-
-// Replace the uri string with your connection string.
-const uri =
-"mongodb+srv://patrick_quickli:41xvaXZNXxb9ZTKp@qa-automation.yjasy2g.mongodb.net/qa-automation?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);*/
